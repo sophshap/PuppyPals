57efb8ad44511049f9d1e2a12ac033040e5e8f35
sophshap <sophieashapiro@gmail.com>
2023-08-06 20:49:36 -0400

    styled in css and completed assignment

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,30 @@ import { puppyList } from './data.js';
 import './App.css';
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
 
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
-        })
-      }
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
